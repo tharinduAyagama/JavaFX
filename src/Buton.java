fa626796e27c47efbfd732106d4ace2e681716a7
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-09-22 13:36:27 +0530

    menu button

--- src/Buton.java
+++ src/Buton.java
@@ -3,6 +3,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -29,7 +32,44 @@ public class Buton extends Application {
                 System.out.println("pressedc");
             }
         });
-        root.getChildren().add(b1);
+
+        Label label = new Label("nothing selected");
+
+        MenuItem item1 = new MenuItem("apple");
+        item1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                label.setText("apple");
+            }
+        });
+
+        MenuItem item2 = new MenuItem("orange");
+        item2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                label.setText("orange");
+            }
+        });
+
+        MenuItem item3 = new MenuItem("grapes");
+        item3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                label.setText("grapes");
+            }
+        });
+
+        MenuItem item4 = new MenuItem("melon");
+        item4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                label.setText("melon");
+            }
+        });
+
+        MenuButton menu = new MenuButton("fruits" , null , item1 , item2 , item3 , item4);
+
+        root.getChildren().addAll(b1 , menu , label);
 
         Scene scene = new Scene(root);
 
