cacd3da8a15c50f4877ab1b53b9a32b07bdc2b39
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-09-22 14:15:00 +0530

    toggle and radio button

--- src/Buton.java
+++ src/Buton.java
@@ -2,10 +2,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -69,7 +66,19 @@ public class Buton extends Application {
 
         MenuButton menu = new MenuButton("fruits" , null , item1 , item2 , item3 , item4);
 
-        root.getChildren().addAll(b1 , menu , label);
+        ToggleButton toggleButton1 = new ToggleButton("yes");
+        ToggleButton toggleButton2 = new ToggleButton("no");
+        ToggleGroup toggleGroup1 = new ToggleGroup();
+        toggleButton1.setToggleGroup(toggleGroup1);
+        toggleButton2.setToggleGroup(toggleGroup1);
+
+        RadioButton radioButton1 = new RadioButton("male");
+        RadioButton radioButton2 = new RadioButton("female");
+        ToggleGroup toggleGroup2 = new ToggleGroup();
+        radioButton1.setToggleGroup(toggleGroup2);
+        radioButton2.setToggleGroup(toggleGroup2);
+
+        root.getChildren().addAll(b1 , menu , label , toggleButton1 , toggleButton2 , radioButton1 , radioButton2);
 
         Scene scene = new Scene(root);
 
